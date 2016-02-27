d804042f76910d944a2e633ce0ef95bd6252e80c
Liam Jack <cuonic@cuonic.com>
2016-02-27 14:29:22 +0100

    Fixing indentation

--- src/PHPAuth.php
+++ src/PHPAuth.php
@@ -8,257 +8,257 @@ namespace PHPAuth;
  */
 
 class PHPAuth {
-	private $database;
-	private $isAuthenticated = false;
-	private $authenticatedUser = NULL;
+    private $database;
+    private $isAuthenticated = false;
+    private $authenticatedUser = NULL;
 
-	public function __construct(Database $database) {
-		$this->database = $database;
+    public function __construct(Database $database) {
+        $this->database = $database;
 
-		if(isset($_COOKIE[Configuration::SESSION_COOKIE_NAME])) {
-			$sessionUuid = $_COOKIE[Configuration::SESSION_COOKIE_NAME];
+        if(isset($_COOKIE[Configuration::SESSION_COOKIE_NAME])) {
+            $sessionUuid = $_COOKIE[Configuration::SESSION_COOKIE_NAME];
 
-			if(!$this->isSessionValid($sessionUuid)) {
-				deleteSessionCookie();
-			}
-		}
-	}
+            if(!$this->isSessionValid($sessionUuid)) {
+                deleteSessionCookie();
+            }
+        }
+    }
 
-	/**
-	 * Allows a user to authenticate and creates a new session
-	 * @param  string 	$email 		User's email address
-	 * @param  string 	$password 	User's password
-	 * @return session
-	 * @throws Exception
-	 */
+    /**
+     * Allows a user to authenticate and creates a new session
+     * @param  string   $email      User's email address
+     * @param  string   $password   User's password
+     * @return session
+     * @throws Exception
+     */
 
-	public function login($email, $password, $isPersistent = false) {
-		if($this->isAuthenticated()) {
-			// User is already authenticated
-			throw new \Exception("already_authenticated");
-		}
+    public function login($email, $password, $isPersistent = false) {
+        if($this->isAuthenticated()) {
+            // User is already authenticated
+            throw new \Exception("already_authenticated");
+        }
 
-		// Validate email address
-		User::validateEmail($email);
+        // Validate email address
+        User::validateEmail($email);
 
-		// Validate password
-		User::validatePassword($password);
+        // Validate password
+        User::validatePassword($password);
 
-		// Get user with provided email address
-		$user = $this->database->getUserByEmail($email);
+        // Get user with provided email address
+        $user = $this->database->getUserByEmail($email);
 
-		if($user == NULL) {
-			// User does not exist
-			throw new \Exception("email_password_incorrect");
-		}
+        if($user == NULL) {
+            // User does not exist
+            throw new \Exception("email_password_incorrect");
+        }
 
-		if(!$user->verifyPassword($password)) {
-			// Provided password doesn't match the user's password
-			throw new \Exception("email_password_incorrect");
-		}
+        if(!$user->verifyPassword($password)) {
+            // Provided password doesn't match the user's password
+            throw new \Exception("email_password_incorrect");
+        }
 
-		// Create a new session
-		$session = Session::createSession($user->getId(), $isPersistent);
+        // Create a new session
+        $session = Session::createSession($user->getId(), $isPersistent);
 
-		// Add session to database
-		$this->database->addSession($session);
+        // Add session to database
+        $this->database->addSession($session);
 
-		// Set the user's session cookie
-		$this->setSessionCookie($session->getUuid(), $session->getExpiryDate());
+        // Set the user's session cookie
+        $this->setSessionCookie($session->getUuid(), $session->getExpiryDate());
 
-		// Set authenticated user
-		$this->setAuthenticatedUser($user);
-	}
+        // Set authenticated user
+        $this->setAuthenticatedUser($user);
+    }
 
-	/**
-	 * Creates a new user account
-	 * @param 	string $email 			User's email address
-	 * @param 	string $password 		User's desired password
-	 * @param 	string $repeatPassword	User's desired password, repeated to prevent typos
-	 * @throws 	Exception
-	 */
+    /**
+     * Creates a new user account
+     * @param   string $email           User's email address
+     * @param   string $password        User's desired password
+     * @param   string $repeatPassword  User's desired password, repeated to prevent typos
+     * @throws  Exception
+     */
 
-	public function register($email, $password, $repeatPassword) {
-		if(!Configuration::REGISTRATION_ENABLED) {
-			throw new \Exception("registration_disabled");
-		}
+    public function register($email, $password, $repeatPassword) {
+        if(!Configuration::REGISTRATION_ENABLED) {
+            throw new \Exception("registration_disabled");
+        }
 
-		if($this->isAuthenticated()) {
-			// User is already authenticated
-			throw new \Exception("already_authenticated");
-		}
+        if($this->isAuthenticated()) {
+            // User is already authenticated
+            throw new \Exception("already_authenticated");
+        }
 
-		// Validate email address
-		User::validateEmail($email);
+        // Validate email address
+        User::validateEmail($email);
 
-		// Validate password
-		User::validatePassword($password);
+        // Validate password
+        User::validatePassword($password);
 
-		if($password !== $repeatPassword) {
-			// Password and password confirmation do not match
-			throw new \Exception("password_no_match");
-		}
+        if($password !== $repeatPassword) {
+            // Password and password confirmation do not match
+            throw new \Exception("password_no_match");
+        }
 
-		if($this->database->doesUserExistByEmail($email)) {
-			// User with this email address already exists
-			throw new \Exception("email_used");
-		}
+        if($this->database->doesUserExistByEmail($email)) {
+            // User with this email address already exists
+            throw new \Exception("email_used");
+        }
 
-		// Create new user
-		$user = User::createUser($email, $password);
+        // Create new user
+        $user = User::createUser($email, $password);
 
-		// Add user to database
-		$this->database->addUser($user);
-	}
+        // Add user to database
+        $this->database->addUser($user);
+    }
 
-	/**
-	 * Changes the authenticated user's password
-	 * @param 	string 	$password
-	 * @param 	string 	$newPassword
-	 * @param 	string 	$repeatNewPassword
-	 * @throws 	Exception
-	 */
+    /**
+     * Changes the authenticated user's password
+     * @param   string  $password
+     * @param   string  $newPassword
+     * @param   string  $repeatNewPassword
+     * @throws  Exception
+     */
 
-	public function changePassword($password, $newPassword, $repeatNewPassword) {
-		if(!$this->isAuthenticated()) {
-			// User is not authenticated
-			throw new \Exception("not_authenticated");
-		}
+    public function changePassword($password, $newPassword, $repeatNewPassword) {
+        if(!$this->isAuthenticated()) {
+            // User is not authenticated
+            throw new \Exception("not_authenticated");
+        }
 
-		$this->authenticatedUser->changePassword($password, $newPassword, $repeatPassword);
-		$this->database->updateUser($this->authenticatedUser);
-	}
+        $this->authenticatedUser->changePassword($password, $newPassword, $repeatPassword);
+        $this->database->updateUser($this->authenticatedUser);
+    }
 
-	/**
-	 * Changes the authenticated user's email address
-	 * @param 	string 	$password
-	 * @param 	string 	$newEmail
-	 * @throws 	Exception
-	 */
+    /**
+     * Changes the authenticated user's email address
+     * @param   string  $password
+     * @param   string  $newEmail
+     * @throws  Exception
+     */
 
-	public function changeEmail($password, $newEmail) {
-		if(!$this->isAuthenticated()) {
-			// User is not authenticated
-			throw new \Exception("not_authenticated");
-		}
+    public function changeEmail($password, $newEmail) {
+        if(!$this->isAuthenticated()) {
+            // User is not authenticated
+            throw new \Exception("not_authenticated");
+        }
 
-		$this->authenticatedUser->changeEmail($password, $newEmail);
-		$this->database->updateUser($this->authenticatedUser);
-	}
+        $this->authenticatedUser->changeEmail($password, $newEmail);
+        $this->database->updateUser($this->authenticatedUser);
+    }
 
-	/**
-	 * Checks whether a user's session is valid or not and performs
-	 * modifications / deletions of sessions when necessary
-	 * @param 	string 	$sessionUuid	The session's UUID
-	 * @return 	bool
-	 */
+    /**
+     * Checks whether a user's session is valid or not and performs
+     * modifications / deletions of sessions when necessary
+     * @param   string  $sessionUuid    The session's UUID
+     * @return  bool
+     */
 
-	public function isSessionValid($sessionUuid) {
-		if($this->isAuthenticated()) {
-			// Session already validated
-			return true;
-		}
+    public function isSessionValid($sessionUuid) {
+        if($this->isAuthenticated()) {
+            // Session already validated
+            return true;
+        }
 
-		// Validate the session's UUID
-		if(!Session::validateUuid($sessionUuid)) {
-			return false;
-		}
+        // Validate the session's UUID
+        if(!Session::validateUuid($sessionUuid)) {
+            return false;
+        }
 
-		// Fetch the session from the database
-		$session = $this->database->getSession($sessionUuid);
+        // Fetch the session from the database
+        $session = $this->database->getSession($sessionUuid);
 
-		if($session == NULL) {
-			// Session doesn't exist
-			return false;
-		}
+        if($session == NULL) {
+            // Session doesn't exist
+            return false;
+        }
 
-		if(!$session->isValid()) {
-			// Session is invalid, delete
-			$this->database->deleteSession($sessionUuid);
-			return false;
-		}
+        if(!$session->isValid()) {
+            // Session is invalid, delete
+            $this->database->deleteSession($sessionUuid);
+            return false;
+        }
 
-		if($session->isUpdateRequired()) {
-			// Session has been updated during verification, push update to database
-			$this->database->updateSession($session);
-		}
+        if($session->isUpdateRequired()) {
+            // Session has been updated during verification, push update to database
+            $this->database->updateSession($session);
+        }
 
-		// Session is valid, set authenticated user
-		$this->setAuthenticatedUserById($session->getUserId());
+        // Session is valid, set authenticated user
+        $this->setAuthenticatedUserById($session->getUserId());
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Indicates if the user is authenticated
-	 * @return 	bool
-	 */
+    /**
+     * Indicates if the user is authenticated
+     * @return  bool
+     */
 
-	public function isAuthenticated() {
-		return $this->isAuthenticated;
-	}
+    public function isAuthenticated() {
+        return $this->isAuthenticated;
+    }
 
-	/**
-	 * Returns the currently authenticated user, or NULL if no user is not authenticated
-	 * @return 	User
-	 */
+    /**
+     * Returns the currently authenticated user, or NULL if no user is not authenticated
+     * @return  User
+     */
 
-	public function getAuthenticatedUser() {
-		return $this->authenticatedUser;
-	}
+    public function getAuthenticatedUser() {
+        return $this->authenticatedUser;
+    }
 
-	/**
-	 * Sets the currently authenticated user
-	 * @param 	User 	$user
-	 */
+    /**
+     * Sets the currently authenticated user
+     * @param   User    $user
+     */
 
-	private function setAuthenticatedUser(User $user) {
-		$this->authenticatedUser = $user;
-		$this->isAuthenticated = true;
-	}
+    private function setAuthenticatedUser(User $user) {
+        $this->authenticatedUser = $user;
+        $this->isAuthenticated = true;
+    }
 
-	/**
-	 * Sets the currently authenticated user by User ID, fetching the user from database
-	 * @param 	int 	$userId
-	 */
+    /**
+     * Sets the currently authenticated user by User ID, fetching the user from database
+     * @param   int     $userId
+     */
 
-	private function setAuthenticatedUserById($userId) {
-		$this->authenticatedUser = $this->database->getUserById($userId);
+    private function setAuthenticatedUserById($userId) {
+        $this->authenticatedUser = $this->database->getUserById($userId);
 
-		if($this->authenticatedUser == NULL) {
-			// User doesn't exist
-			$this->isAuthenticated = false;
-		} else {
-			$this->isAuthenticated = true;
-		}
-	}
+        if($this->authenticatedUser == NULL) {
+            // User doesn't exist
+            $this->isAuthenticated = false;
+        } else {
+            $this->isAuthenticated = true;
+        }
+    }
 
-	/**
-	 * Sets the user's session cookie
-	 * @param 	string 	$sessionUuid
-	 * @param 	int 	$expiryDate
-	 */
+    /**
+     * Sets the user's session cookie
+     * @param   string  $sessionUuid
+     * @param   int     $expiryDate
+     */
 
-	public function setSessionCookie($sessionUuid, $expiryDate) {
-		setcookie(
-			Configuration::SESSION_COOKIE_NAME,
-			$sessionUuid,
-			$expiryDate,
-			Configuration::SESSION_COOKIE_PATH,
-			Configuration::SESSION_COOKIE_DOMAIN,
-			Configuration::SESSION_COOKIE_SECURE,
-			Configuration::SESSION_COOKIE_HTTPONLY
-		);
-	}
+    public function setSessionCookie($sessionUuid, $expiryDate) {
+        setcookie(
+            Configuration::SESSION_COOKIE_NAME,
+            $sessionUuid,
+            $expiryDate,
+            Configuration::SESSION_COOKIE_PATH,
+            Configuration::SESSION_COOKIE_DOMAIN,
+            Configuration::SESSION_COOKIE_SECURE,
+            Configuration::SESSION_COOKIE_HTTPONLY
+        );
+    }
 
-	/**
-	 * Deletes the user's session cookie
-	 */
+    /**
+     * Deletes the user's session cookie
+     */
 
-	public function deleteSessionCookie() {
-		unset($_COOKIE[Configuration::SESSION_COOKIE_NAME]);
-		setSessionCookie(NULL, time() - 3600);
-	}
+    public function deleteSessionCookie() {
+        unset($_COOKIE[Configuration::SESSION_COOKIE_NAME]);
+        setSessionCookie(NULL, time() - 3600);
+    }
 
 
 }
--- src/User.php
+++ src/User.php
@@ -8,205 +8,205 @@ namespace PHPAuth;
  */
 
 class User {
-	private $id;
-	private $email;
-	private $password;
+    private $id;
+    private $email;
+    private $password;
 
-	public function __construct($id, $email, $password) {
-		$this->id = $id;
-		$this->email = $email;
-		$this->password = $password;
-	}
+    public function __construct($id, $email, $password) {
+        $this->id = $id;
+        $this->email = $email;
+        $this->password = $password;
+    }
 
-	/**
-	 * Returns the user's ID
-	 * @return 	int
-	 */
+    /**
+     * Returns the user's ID
+     * @return  int
+     */
 
-	public function getId() {
-		return $this->id;
-	}
+    public function getId() {
+        return $this->id;
+    }
 
-	/**
-	 * Returns the user's email address
-	 * @return 	string
-	 */
+    /**
+     * Returns the user's email address
+     * @return  string
+     */
 
-	public function getEmail() {
-		return $this->email;
-	}
+    public function getEmail() {
+        return $this->email;
+    }
 
-	/**
-	 * Modifies the user's email address
-	 * @param 	string $email 	User's email address
-	 */
+    /**
+     * Modifies the user's email address
+     * @param   string $email   User's email address
+     */
 
-	private function setEmail($email) {
-		$this->email = $email;
-	}
+    private function setEmail($email) {
+        $this->email = $email;
+    }
 
-	/**
-	 * Returns the user's hashed password
-	 * @return 	string
-	 */
+    /**
+     * Returns the user's hashed password
+     * @return  string
+     */
 
-	public function getPassword() {
-		return $this->password;
-	}
+    public function getPassword() {
+        return $this->password;
+    }
 
-	/**
-	 * Modifies the user's hashed password
-	 * @param 	string 	$password 	User's hashed password
-	 * @throws 	Exception
-	 */
+    /**
+     * Modifies the user's hashed password
+     * @param   string  $password   User's hashed password
+     * @throws  Exception
+     */
 
-	private function setPassword($password) {
-		if(strlen($password) != 60) {
-			throw new \Exception("system_error");
-		}
+    private function setPassword($password) {
+        if(strlen($password) != 60) {
+            throw new \Exception("system_error");
+        }
 
-		$this->password = $password;
-	}
+        $this->password = $password;
+    }
 
-	/**
-	 * Validates an email address
-	 * @param 	string 	$email
-	 * @throws 	Exception
-	 */
+    /**
+     * Validates an email address
+     * @param   string  $email
+     * @throws  Exception
+     */
 
-	public static function validateEmail($email) {
-		if(strlen($email) == 0) {
-			throw new \Exception("email_empty");
-		}
+    public static function validateEmail($email) {
+        if(strlen($email) == 0) {
+            throw new \Exception("email_empty");
+        }
 
-		if(strlen($email) < Configuration::EMAIL_MINIMUM_LENGTH) {
-			throw new \Exception("email_short");
-		}
+        if(strlen($email) < Configuration::EMAIL_MINIMUM_LENGTH) {
+            throw new \Exception("email_short");
+        }
 
-		if(strlen($email) > Configuration::EMAIL_MAXIMUM_LENGTH) {
-			throw new \Exception("email_long");
-		}
+        if(strlen($email) > Configuration::EMAIL_MAXIMUM_LENGTH) {
+            throw new \Exception("email_long");
+        }
 
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			throw new \Exception("email_invalid");
-		}
-	}
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \Exception("email_invalid");
+        }
+    }
 
-	/**
-	 * Validates a password
-	 * @param 	string 	$password
-	 * @throws 	Exception
-	 */
+    /**
+     * Validates a password
+     * @param   string  $password
+     * @throws  Exception
+     */
 
-	public static function validatePassword($password) {
-		if(strlen($password) == 0) {
-			throw new \Exception("password_empty");
-		}
+    public static function validatePassword($password) {
+        if(strlen($password) == 0) {
+            throw new \Exception("password_empty");
+        }
 
-		if(strlen($password) < Configuration::PASSWORD_MINIMUM_LENGTH) {
-			throw new \Exception("password_short");
-		}
+        if(strlen($password) < Configuration::PASSWORD_MINIMUM_LENGTH) {
+            throw new \Exception("password_short");
+        }
 
-		if(strlen($password) > Configuration::PASSWORD_MAXIMUM_LENGTH) {
-			throw new \Exception("password_long");
-		}
+        if(strlen($password) > Configuration::PASSWORD_MAXIMUM_LENGTH) {
+            throw new \Exception("password_long");
+        }
 
-		$zxcvbn = new \ZxcvbnPhp\Zxcvbn();
-		$score = $zxcvbn->passwordStrength($password)['score'];
+        $zxcvbn = new \ZxcvbnPhp\Zxcvbn();
+        $score = $zxcvbn->passwordStrength($password)['score'];
 
-		if($score < Configuration::PASSWORD_MINIMUM_SCORE) {
-			throw new \Exception("password_weak");
-		}
-	}
+        if($score < Configuration::PASSWORD_MINIMUM_SCORE) {
+            throw new \Exception("password_weak");
+        }
+    }
 
-	/**
-	 * Changes a user's password
-	 * @param 	string 	$password 			User's current password
-	 * @param 	string 	$newPassword 		User's desired new password
-	 * @param 	string 	$repeatNewPassword	User's desired new password, repeated to prevent typos
-	 * @throws 	Exception
-	 */
+    /**
+     * Changes a user's password
+     * @param   string  $password           User's current password
+     * @param   string  $newPassword        User's desired new password
+     * @param   string  $repeatNewPassword  User's desired new password, repeated to prevent typos
+     * @throws  Exception
+     */
 
-	public function changePassword($password, $newPassword, $repeatNewPassword) {
-		// Validate current password
-		self::validatePassword($password);
+    public function changePassword($password, $newPassword, $repeatNewPassword) {
+        // Validate current password
+        self::validatePassword($password);
 
-		// Validate new password
-		self::validatePassword($newPassword, $repeatNewPassword);
+        // Validate new password
+        self::validatePassword($newPassword, $repeatNewPassword);
 
-		if($newPassword !== $repeatNewPassword) {
-			// New password and confirmation do not match
-			throw new \Exception("password_no_match");
-		}
+        if($newPassword !== $repeatNewPassword) {
+            // New password and confirmation do not match
+            throw new \Exception("password_no_match");
+        }
 
-		if(!$this->verifyPassword($password)) {
-			// User's current password is incorrect
-			throw new \Exception("password_incorrect");
-		}
+        if(!$this->verifyPassword($password)) {
+            // User's current password is incorrect
+            throw new \Exception("password_incorrect");
+        }
 
-		// Hash new password
-		$newPassword = self::hashPassword($newPassword);
+        // Hash new password
+        $newPassword = self::hashPassword($newPassword);
 
-		// Change password
-		$this->setPassword($password);
-	}
+        // Change password
+        $this->setPassword($password);
+    }
 
-	/**
-	 * Changes a user's email address
-	 * @param 	string 	$password 	User's password
-	 * @param 	string 	$newEmail 	User's new email address
-	 * @throws 	Exception
-	 */
+    /**
+     * Changes a user's email address
+     * @param   string  $password   User's password
+     * @param   string  $newEmail   User's new email address
+     * @throws  Exception
+     */
 
-	public function changeEmail($password, $newEmail) {
-		// Validate password
-		self::validatePassword($password);
+    public function changeEmail($password, $newEmail) {
+        // Validate password
+        self::validatePassword($password);
 
-		// Validate email address
-		self::validateEmail($newEmail);
+        // Validate email address
+        self::validateEmail($newEmail);
 
-		if($newEmail == $this->getEmail()) {
-			// New email address is the same as current email address
-			throw new \Exception("email_same");
-		}
+        if($newEmail == $this->getEmail()) {
+            // New email address is the same as current email address
+            throw new \Exception("email_same");
+        }
 
-		if(!$this->verifyPassword($password)) {
-			// User's current password is incorrect
-			throw new \Exception("password_incorrect");
-		}
+        if(!$this->verifyPassword($password)) {
+            // User's current password is incorrect
+            throw new \Exception("password_incorrect");
+        }
 
-		// Change email address
-		$this->setEmail($newEmail);
-	}
+        // Change email address
+        $this->setEmail($newEmail);
+    }
 
-	/**
-	 * Hashes a password
-	 * @param 	string 	$password
-	 * @return 	string
-	 */
+    /**
+     * Hashes a password
+     * @param   string  $password
+     * @return  string
+     */
 
-	public static function hashPassword($password) {
-		return password_hash($password, PASSWORD_BCRYPT, array("cost" => Configuration::PASSWORD_HASH_COST));
-	}
+    public static function hashPassword($password) {
+        return password_hash($password, PASSWORD_BCRYPT, array("cost" => Configuration::PASSWORD_HASH_COST));
+    }
 
-	/**
-	 * Check if a password matches the user's password
-	 * @param 	string 	$password
-	 * @return 	bool
-	 */
+    /**
+     * Check if a password matches the user's password
+     * @param   string  $password
+     * @return  bool
+     */
 
-	public function verifyPassword($password) {
-		return password_verify($password, $this->password);
-	}
+    public function verifyPassword($password) {
+        return password_verify($password, $this->password);
+    }
 
-	/**
-	 * Returns a new user
-	 * @param 	string 	$email
-	 * @param 	string 	$password
-	 * @return 	User
-	 */
+    /**
+     * Returns a new user
+     * @param   string  $email
+     * @param   string  $password
+     * @return  User
+     */
 
-	public static function createUser($email, $password) {
-		return new User($email, self::hashPassword($password), NULL);
-	}
+    public static function createUser($email, $password) {
+        return new User($email, self::hashPassword($password), NULL);
+    }
 }
